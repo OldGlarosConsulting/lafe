a7b9ea75fee4be130c23f8cabab3659ec692bf08
Cesar Vieira <cesar.murilo@glaros.com.br>
2021-02-24 23:20:18 -0300

    cesar

---
+++ packages/web/src/mocks/macro-dashboard/33/january/tables/general.tsx
@@ -0,0 +1,22 @@
+import IGeneralTable from '@/interfaces/builds/IGeneralTable';
+
+export default [
+  {
+    month: 'jan-21',
+    estimated_cumultative_physics: 0.8016,
+    cumulative_physics_executed: 0.7217,
+    expected_monthly_physical: 0.0588,
+    physical_monthly_executed: 0.0349,
+    physical_deviation: -0.0799,
+    performed_monthly_physicist: -38,
+  },
+  {
+    month: 'dez-20',
+    estimated_cumultative_physics: 0.7428,
+    cumulative_physics_executed: 0.6868,
+    expected_monthly_physical: 0.0904,
+    physical_monthly_executed: 0.0510,
+    physical_deviation: -0.0560,
+    performed_monthly_physicist: -19,
+  },
+] as IGeneralTable[];
---
+++ packages/web/src/mocks/macro-dashboard/33/january/tables/summary-of-main-activities/late-activities.ts
@@ -0,0 +1,74 @@
+import IMainActivities from '@/interfaces/builds/IMainActivities';
+
+export default [
+  {
+    item: '13',
+    activity: 'ESQUADRIAS DE ALUMÍNIO',
+    prev: 0.844094560380426,
+    real: 0.244316227981346,
+    physical_deviation: -0.59977833239908,
+  },
+  {
+    item: '09',
+    activity: 'REVESTIMENTO DE PISO',
+    prev: 0.740248702719677,
+    real: 0.411500234176305,
+    physical_deviation: -0.328748468543372,
+  },
+  {
+    item: '17',
+    activity: 'BANCADAS',
+    prev: 0.956507423066814,
+    real: 0.356019910338888,
+    physical_deviation: -0.600487512727926,
+  },
+  {
+    item: '11',
+    activity: 'REVESTIMENTO DE TETO',
+    prev: 0.954854775483966,
+    real: 0.666716235862322,
+    physical_deviation: -0.288138539621644,
+  },
+  {
+    item: '10',
+    activity: 'REVESTIMENTO DE PAREDE',
+    prev: 0.756779638028001,
+    real: 0.684157483940392,
+    physical_deviation: -0.726221540876086,
+  },
+  {
+    item: '15',
+    activity: 'ESQUADRIAS EM MADEIRA',
+    prev: 0.258935821301514,
+    real: null,
+    physical_deviation: -0.258935821301514,
+  },
+  {
+    item: '19',
+    activity: 'PINTURA',
+    prev: 0.39892362009256,
+    real: 0.308731260911798,
+    physical_deviation: -0.901923591807618,
+  },
+  {
+    item: '12',
+    activity: 'REVESTIMENTO EXTERNO',
+    prev: 0.985326814554542,
+    real: 0.951812825831941,
+    physical_deviation: -0.335139887226015,
+  },
+  {
+    item: '18',
+    activity: 'LOUÇAS E METAIS',
+    prev: 0.00626966072674459,
+    real: 0.00454232641154454,
+    physical_deviation: -0.00172733431520005,
+  },
+  {
+    item: '08',
+    activity: 'IMPERMEABILIZAÇÃO / ISOLAMENTO',
+    prev: 100.0,
+    real: 0.98173187163861,
+    physical_deviation: -0.0182681283613902,
+  },
+] as IMainActivities[];
