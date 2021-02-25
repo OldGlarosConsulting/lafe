1d5e67b0f3f492011f991f0613636a0a269b106c
Cesar Vieira <cesar.murilo@glaros.com.br>
2021-02-24 23:39:33 -0300

    Revert "cesar"
    This reverts commit 965eaa08c937790504da34a6941e858607ddde8f.

--- packages/web/src/mocks/macro-dashboard/33/build.ts
+++ packages/web/src/mocks/macro-dashboard/33/build.ts
@@ -3,10 +3,9 @@ import IMonthBuild from '@/interfaces/builds/IMacroDasboardDataBuilds';
 import novemberBuild from './november/november-build';
 import octoberBuild from './october/october-build';
 import septemberBuild from './september/september-build';
-import januaryBuild from './january/january-build';
 
 export default {
   id: 'build33',
   build: 'Obra 33',
-  months: [septemberBuild, octoberBuild, novemberBuild, januaryBuild],
+  months: [septemberBuild, octoberBuild, novemberBuild],
 } as IMonthBuild;
