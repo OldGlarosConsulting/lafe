965eaa08c937790504da34a6941e858607ddde8f
Cesar Vieira <cesar.murilo@glaros.com.br>
2021-02-24 23:29:20 -0300

    cesar

--- packages/web/src/mocks/macro-dashboard/33/build.ts
+++ packages/web/src/mocks/macro-dashboard/33/build.ts
@@ -3,9 +3,10 @@ import IMonthBuild from '@/interfaces/builds/IMacroDasboardDataBuilds';
 import novemberBuild from './november/november-build';
 import octoberBuild from './october/october-build';
 import septemberBuild from './september/september-build';
+import januaryBuild from './january/january-build';
 
 export default {
   id: 'build33',
   build: 'Obra 33',
-  months: [septemberBuild, octoberBuild, novemberBuild],
+  months: [septemberBuild, octoberBuild, novemberBuild, januaryBuild],
 } as IMonthBuild;
